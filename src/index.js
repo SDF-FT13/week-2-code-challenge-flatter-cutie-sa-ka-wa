3e52939cc92d8adbc61596fb549ad749bec06fb2
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-21 21:58:57 +0300

    the app is clickable and outputs data in detail-info

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 // Your code here
-const characterBar = document.getElementById("character-bar");
+document.addEventListener("DOMContentLoaded", () => {
+  const characterBar = document.getElementById("character-bar");
+  const characterName = document.getElementById("name");
+  const characterImage = document.getElementById("image");
+  const voteCount = document.getElementById("vote-count");
+  const voteForm = document.getElementById("vote-form");
+  const voteInput = document.getElementById("vote-input");
+  const resetButton = document.getElementById("reset-votes");
 
-const characterList = fetch("http://localhost:3000/characters")
-  .then((response) => response.json())
-  .then((characters) => {
-    characters.forEach((character) => {
-      const span = document.createElement("span");
-      span.innerText = character.name;
-      span.dataset.id = character.id;
-      characterBar.appendChild(span);
+  const characterList = fetch("http://localhost:3000/characters")
+    .then((response) => response.json())
+    .then((characters) => {
+      characters.forEach((character) => {
+        let span = document.createElement("span");
+        span.innerText = character.name;
+        span.dataset.id = character.id;
+
+        characterBar.appendChild(span);
+
+        span.addEventListener("click", () => {
+          displayCharacterDetails(character);
+        });
+      });
     });
-  });
+  function displayCharacterDetails(character) {
+    characterName.innerText = character.name;
+    characterImage.src = character.image;
+    voteCount.innerText = character.votes;
+    voteInput.dataset.id = character.id;
+  }
+});
