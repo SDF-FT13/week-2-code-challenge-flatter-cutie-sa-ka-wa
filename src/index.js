fb0d20b15ae74f9424794cbbe7eaa488cc30e305
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-23 20:31:38 +0300

    the app outputs the total value

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const characterName = document.getElementById("name");
   const characterImage = document.getElementById("image");
   const voteCount = document.getElementById("vote-count");
-  const voteForm = document.getElementById("vote-form");
-  const voteInput = document.getElementById("vote-input");
-  const resetButton = document.getElementById("reset-votes");
+  const voteForm = document.getElementById("votes-form");
+  const voteInput = document.getElementById("votes");
+  const resetButton = document.getElementById("reset-btn");
 
   const characterList = fetch("http://localhost:3000/characters")
     .then((response) => response.json())
@@ -26,7 +26,28 @@ document.addEventListener("DOMContentLoaded", () => {
   function displayCharacterDetails(character) {
     characterName.innerText = character.name;
     characterImage.src = character.image;
-    voteCount.innerText = character.votes;
+    characterImage.alt = character.name;
+    voteCount.innerText = ` ${character.votes}`;
     voteInput.dataset.id = character.id;
   }
+
+  voteForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let votesToAdd = parseInt(voteInput.value, 10);
+    let currentVotes = parseInt(
+      voteCount.innerText.replace("Total Votes: ", ""),
+      10
+    );
+
+    if (!isNaN(votesToAdd) && votesToAdd > 0) {
+      voteCount.innerText = ` ${currentVotes + votesToAdd}`;
+    }
+
+    voteForm.reset();
+  });
+
+  resetButton.addEventListener("click", () => {
+    voteCount.innerText = 0;
+  });
 });
